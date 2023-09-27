cc313148ee26e147fce00a0c4ba0fc6c910574f3
ianfc15 <ianfcampos5@gmail.com>
2023-09-27 20:17:15 -0300

    ListaAvaliativa1

--- ListaAvaliativa1/05_ValorMultiplo.c
+++ ListaAvaliativa1/05_ValorMultiplo.c
@@ -2,30 +2,35 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
     int n1, n2;
 
-    // printf("Digite dois valores:\n");
     scanf("%d", &n1);
     scanf("%d", &n2);
 
-    if (n1 == n2){
-        printf("Os valores lidos sao iguais.\n");
-    }else if(n1 > n2){
-        printf("%d eh maior que %d\n", n1, n2);
-        if(n1 % n2 == 0){
-            printf("%d eh multiplo de %d\n", n1 ,n2);
-        }else if(n1 % n2 != 0){
-            printf("%d nao eh multiplo de  %d\n", n1 ,n2);
-        }        
-    }else if(n1 < n2){  
+    if(n1 == n2){
+        printf("Os valores lidos sao iguais\n");
+    }else{
+        if(n1 > n2){
+            printf("%d eh maior que %d\n", n1, n2);
+            if(n1 == 0 || n2 == 0){
+                printf("%d nao eh multiplo de %d\n", n1, n2);
+            }else if(n1 % n2 == 0){
+                printf("%d eh multiplo de %d\n", n1, n2);
+            }else{
+                printf("%d nao eh multiplo de %d\n", n1, n2);
+            }
+        }else{
             printf("%d eh maior que %d\n", n2, n1);
-        if(n2 % n1 == 0){
-            printf("%d eh multiplo de %d\n", n2 ,n1);
-        }else if(n2 % n1 != 0){
-            printf("%d nao eh multiplo de  %d\n", n2 ,n1);
+            if(n2 == 0 || n1 == 0){
+                printf("%d nao eh multiplo de %d\n", n2, n1);
+            }else if(n2 % n1 == 0){
+                printf("%d eh multiplo de %d\n", n2, n1);
+            }else{
+                printf("%d nao eh multiplo de %d\n", n2, n1);
+            }
         }
     }
-        
+
     return 0;
 }
