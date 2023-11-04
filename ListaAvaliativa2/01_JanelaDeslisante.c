e7aca3b91844b0de3031c43659e945603b816eb6
ianfc15 <ianfcampos5@gmail.com>
2023-11-04 17:51:42 -0300

    ListaAvaliativa2

--- ListaAvaliativa2/01_JanelaDeslisante.c
+++ ListaAvaliativa2/01_JanelaDeslisante.c
@@ -1,32 +1,35 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+    #include <stdio.h>
+    #include <math.h>
+    #include <stdlib.h>
 
-int main(){
-    int n[10], n1[10];
-    int i = 0, j = 0, soma = 0;
+    int main(){
+        int n[10], n1[10];
+        int i = 0, j = 0, k = 0, soma = 0;
 
-    for(i = 0; i <= 9; i++){
-        scanf("%d", &n[i]);
-    }
+        for(i = 0; i <= 9; i++){
+            scanf("%d", &n[i]);
+        }
 
-    for(i = 0; i <= 9; i++){
-        printf("%d ", n[i]);
-    }
-    printf("\n");
-
-    // Parte da repeticao
-
-    int a = 8;
-    for(i = 0; i <= 9; i++){
-        for(j = 0; j <= a; j++){
-            soma = n[j] + n[j + 1];
-            n[j] = soma;
-            printf(" %d", n[j]);
+        for(i = 0; i <= 9; i++){
+            printf("%d ", n[i]);
         }
         printf("\n");
-        a--;     
-    }
 
-    return 0;
-}
+        // Parte da repeticao
+
+        int a = 8;
+        for(i = 0; i <= 9; i++){
+            for(j = 0; j <= a; j++){
+                soma = n[j] + n[j + 1];
+                n[j] = soma;
+                printf("%d", n[j]);
+                if(a != 2){
+                    printf(" ");
+                }
+            }
+            printf("\n");
+            a--;     
+        }
+
+        return 0;
+    }
