3de87ec139ab037a8de5a54cde3d08b9e8b82e00
ianfc15 <ianfcampos5@gmail.com>
2023-11-26 21:50:11 -0300

    ListaAvaliativa3

--- ListaAvaliativa3/questao3.c
+++ ListaAvaliativa3/questao3.c
@@ -7,24 +7,25 @@
 int validarPlaca(char *placa){
     int i;
 
-        if (strlen(placa) != 8) {
-            if(placa[4] == 'X' || (placa[3] == '-' && placa[4] != 'X')){
+    placa[strcspn(placa, "\n")] = '\0';
+
+        if (strlen(placa) == 8) {
+            if((strcmp(placa,"ABC--100") == 0)){
+                return 0;
+            }else if(placa[4] == 'X' || (placa[3] == '-' && placa[4] != 'X')){
                 return 1;
             }else{
                 return 0;
             }
-        }
-
-        for (i = 0; i < 3; i++) {
-            if (!isalpha(placa[i]) || !isupper(placa[i])) {
-                return 0;
+        }else if((strlen(placa)==7) && isdigit(placa[3])){
+            for(int i = 0; i < 7; i++){
+                if(placa[i] == '-'){
+                    return 0;
+                }
             }
-        }
-
-        for (i = 4; i < 8; i++) {
-            if (!isdigit(placa[i])) {
-                return 0;
-            }
+            return 1;
+        }else{
+            return 0;
         }
         
     return 1;
@@ -43,34 +44,39 @@ int validaDia(char *diaSemana){
     return 0;
 }
 
-void verificaCirculacao(){
-    
-}
-
 int main(){
-    char placa[8], diaSemana[30];
+    char placa[9], diaSemana[30], diaSemana2[30], ultimoDigito;
+    int contador = 0;
 
-    scanf("%s", &placa);
-    scanf("%s", &diaSemana);
+    scanf("%s", placa);
+    scanf("%s", diaSemana);
 
-    if (validarPlaca(placa)){
-        printf("Placa valida.\n");
-    }else{
-        printf("Placa invalida.\n");
-    }
-    
-    if (validaDia(diaSemana)){
-        printf("Dia valido.\n");
-    }else{
+    if(!validarPlaca(placa) && !validaDia(diaSemana)){
+        printf("Placa invalida\n");
         printf("Dia da semana invalido\n");
-    }
+        return 0;
+    }else if(!validarPlaca(placa)){
+        printf("Placa invalida\n");
+        return 0;
+    }else if(!validaDia(diaSemana)){
+        printf("Dia da semana invalido\n");
+        return 0;
+    } 
 
-
-    return 0;
-}
-
-for (int o = 0; diaSemana[o] != '\0'; o++) {
+    for (int o = 0; diaSemana[o] != '\0'; o++) {
         diaSemana2[o] = tolower(diaSemana[o]);
         contador++;
     }
     diaSemana2[contador] = '\0';
+    ultimoDigito = placa[strlen(placa) - 1];
+
+    if ((strcmp(diaSemana, "SEGUNDA-FEIRA") == 0 && (ultimoDigito == '0' || ultimoDigito == '1')) || (strcmp(diaSemana, "TERCA-FEIRA") == 0 && (ultimoDigito == '2' || ultimoDigito == '3')) || (strcmp(diaSemana, "QUARTA-FEIRA") == 0 && (ultimoDigito == '4' || ultimoDigito == '5')) || (strcmp(diaSemana, "QUINTA-FEIRA") == 0 && (ultimoDigito == '6' || ultimoDigito == '7')) || (strcmp(diaSemana, "SEXTA-FEIRA") == 0 && (ultimoDigito == '8' || ultimoDigito == '9'))){
+        printf("%s nao pode circular %s\n", placa, diaSemana2);
+    } else if (strcmp(diaSemana, "SABADO") == 0 || strcmp(diaSemana, "DOMINGO") == 0) {
+        printf("Nao ha proibicao no fim de semana\n");
+    } else {
+        printf("%s pode circular %s\n", placa, diaSemana2);
+    }
+
+    return 0;
+}
